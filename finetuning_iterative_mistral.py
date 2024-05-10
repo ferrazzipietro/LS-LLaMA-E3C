3c4caea485c71081d272506db63568b799f2f6fb
ferrazzipietro <ferrazzipietro@gmail.com>
2024-05-10 13:54:33 +0200

    wandb

--- finetuning_iterative_mistral.py
+++ finetuning_iterative_mistral.py
@@ -67,7 +67,7 @@ def main(ADAPTERS_CHECKPOINT,
     # Monitering the LLM
     wandb.login(key = WANDB_KEY)
     run = wandb.init(project=config.WANDB_PROJECT_NAME, job_type="training", anonymous="allow",
-                    name=config.WANDB_RUN_NAME,
+                    name=ADAPTERS_CHECKPOINT.split('/')[1],
                     config={'model': config.BASE_MODEL_CHECKPOINT, 
                             'dataset': config.DATASET_CHEKPOINT, 
                             'layer': config.TRAIN_LAYER,
