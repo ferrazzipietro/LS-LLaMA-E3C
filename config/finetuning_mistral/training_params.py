a7589b08f0a4c2ad58a3d99e16eb643fa91daf24
ferrazzipietro <ferrazzipietro@gmail.com>
2024-05-10 13:50:51 +0200

    wandb

--- config/finetuning_mistral/training_params.py
+++ config/finetuning_mistral/training_params.py
@@ -3,9 +3,6 @@ num_train_epochs= 3
 per_device_train_batch_size= 8
 gradient_accumulation_steps= [2,4,8] # reduce the number to finish faster
 optim = "paged_adamw_8bit"
-save_steps= 1000
-logging_strategy="steps"
-logging_steps= 10
 learning_rate= [2e-4]
 weight_decay= 0.001
 fp16= False 
@@ -16,7 +13,17 @@ warmup_ratio= 0.3
 group_by_length= True
 lr_scheduler_type= "constant"
 
-
+save_strategy="steps"
+logging_steps=50
+logging_strategy="steps"
+evaluation_strategy= "steps"
+save_strategy=evaluation_strategy
+save_steps= 25
+eval_steps=save_steps
+greater_is_better=False
+metric_for_best_model="eval_loss"
+save_total_limit = 1
+load_best_model_at_end = True
 
 ### SFTTrainer
 """
