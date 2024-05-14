97f5977068291d0e41c1122e27de93383efb404e
ferrazzipietro <ferrazzipietro@gmail.com>
2024-05-14 17:44:14 +0200

    mistral masked token

--- src/billm/__init__.py
+++ src/billm/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from .modeling_llama import *  # NOQA
-from .modeling_mistralbillm import *  # NOQA
+# from .modeling_mistralbillm import *  # NOQA
 from .modeling_qwen2 import *  # NOQA
+from .modeling_mistral import *
 
 
 __version__ = '0.1.3'
