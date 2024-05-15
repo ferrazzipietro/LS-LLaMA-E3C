cb09a5e286a7564aac7a3d95fc83a0763bbf3c78
ferrazzipietro <ferrazzipietro@gmail.com>
2024-05-15 17:31:00 +0200

    tmp

--- generate_model_predictions.py
+++ generate_model_predictions.py
@@ -82,16 +82,16 @@ login(token=HF_TOKEN_WRITE)
 
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:512"
 torch.cuda.empty_cache() 
-adapters_list = generate_adapters_list('llama', appendix='5Epochs')
-# generate_model_predictions(adapters_list, batch_size=2)
-batch_size = 2
+
+
+print('PREPROCESSING DATA...')
 DATASET_CHEKPOINT="ferrazzipietro/e3c-sentences" 
 TRAIN_LAYER="en.layer1"
+adapters_list = generate_adapters_list('llama', appendix='5Epochs')
 peft_config = PeftConfig.from_pretrained(adapters_list[0], token = HF_TOKEN_WRITE)
 BASE_MODEL_CHECKPOINT = peft_config.base_model_name_or_path
 tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL_CHECKPOINT,token =HF_TOKEN_WRITE)
 tokenizer.pad_token = tokenizer.eos_token
-print('PREPROCESSING DATA...')
 dataset = load_dataset(DATASET_CHEKPOINT, token=HF_TOKEN_WRITE) #download_mode="force_redownload"
 dataset = dataset[TRAIN_LAYER]
 dataset = dataset.shuffle(seed=1234)  
@@ -108,25 +108,36 @@ preprocessor = DataPreprocessor()
 _, data, _ = preprocessor.split_layer_into_train_val_test_(tokenized_ds, TRAIN_LAYER)
 print('PREPROCESSING DATA...DONE')
 
+
+
+
+
+print('LOADING MODEL...')
+model_type = 'llama' if 'llama' in BASE_MODEL_CHECKPOINT.lower() else 'mistral'
+if model_type == 'llama':
+    ModelForTokenClassification = LlamaForTokenClassification
+elif model_type == 'mistral':
+    ModelForTokenClassification = MistralForTokenClassification
+else:
+    raise ValueError('Model type not recognized')
+base_model = ModelForTokenClassification.from_pretrained(
+    BASE_MODEL_CHECKPOINT,
+    num_labels=len(label2id), id2label=id2label, label2id=label2id,
+    token = HF_TOKEN_WRITE,
+    # cache_dir='/data/disk1/share/pferrazzi/.cache',
+    device_map='auto',
+    # quantization_config = bnb_config
+    )
+print('LOADING MODEL...DONE')
+
+
+batch_size = 2
+
 for adapters in adapters_list:
-    model_type = 'llama' if 'llama' in adapters.lower() else 'mistral'
-    print('loading model with adapters:', adapters)
-    if model_type == 'llama':
-        ModelForTokenClassification = LlamaForTokenClassification
-    elif model_type == 'mistral':
-        ModelForTokenClassification = MistralForTokenClassification
-    else:
-        raise ValueError('Model type not recognized')
+    
     peft_config = PeftConfig.from_pretrained(adapters, token = HF_TOKEN_WRITE)
     BASE_MODEL_CHECKPOINT = peft_config.base_model_name_or_path
-    base_model = ModelForTokenClassification.from_pretrained(
-        peft_config.base_model_name_or_path,
-        num_labels=len(label2id), id2label=id2label, label2id=label2id,
-        token = HF_TOKEN_WRITE,
-        # cache_dir='/data/disk1/share/pferrazzi/.cache',
-        device_map='auto',
-        # quantization_config = bnb_config
-        )
+
     model = PeftModel.from_pretrained(base_model, adapters, token = HF_TOKEN_WRITE)
     model = model.merge_and_unload()
     print('DONE')
@@ -135,6 +146,5 @@ for adapters in adapters_list:
     print(test_data)
     test_data.push_to_hub(adapters, token=HF_TOKEN_WRITE, split='test')
     del model
-    del base_model
     gc.collect()
     torch.cuda.empty_cache()
