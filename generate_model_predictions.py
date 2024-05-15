13abc75115064e796cc734933fce313533a07744
ferrazzipietro <ferrazzipietro@gmail.com>
2024-05-15 16:02:51 +0200

    mistral masked token

--- generate_model_predictions.py
+++ generate_model_predictions.py
@@ -4,6 +4,10 @@ from dotenv import dotenv_values
 
 from src.billm import LlamaForTokenClassification as ModelForTokenClassification
 
+from huggingface_hub import login
+HF_TOKEN_WRITE = dotenv_values(".env.base")['HF_TOKEN_WRITE']
+login(token=HF_TOKEN_WRITE)
+
 HF_TOKEN = dotenv_values(".env.base")['HF_TOKEN']
 adapters_list = generate_adapters_list('llama', appendix='5Epochs')
 generate_model_predictions(adapters_list[:2])
