6b9f673d6bcfacd0319cd1393c2c38b9fd6f21a5
ferrazzipietro <ferrazzipietro@gmail.com>
2024-05-18 17:46:09 +0200

    3EpochsLast bfloat16

--- model_predictions_generation.py
+++ model_predictions_generation.py
@@ -15,7 +15,7 @@ from src.billm import LlamaForTokenClassification, MistralForTokenClassification
 
 
 batch_size = 64
-appendix = '3EpochsLast.py' # '5EpochsBestF1Train' # 5EpochsBestF1Train
+appendix = '3EpochsLast' # '5EpochsBestF1Train' # 5EpochsBestF1Train
 log_name_training = "llama_3EpochsLast"#"noLora_3EpochsLast"
 training_type= ''#'NoLora' # ''
 
