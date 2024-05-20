e144f38b9dd58689f001a46e449741394bd2cf5a
ferrazzipietro <ferrazzipietro@gmail.com>
2024-05-20 18:31:21 +0200

    tmp

--- utils/evaluator.py
+++ utils/evaluator.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datasets import Dataset
 
 class Evaluator():
     def __init__(self, data, tokenizer):
@@ -27,13 +28,30 @@ class Evaluator():
             TP = TP + (1 if pred == lab and lab!='O' else 0)
             FP = FP + (1 if pred != lab and lab =='O' else 0)
             FN = FN + (1 if pred != lab and pred =='O' else 0)
+        TN = len(predictions) - (TP + FP + FN) 
+        try:
+            precision = TP / (TP + FP)
+        except:
+            precision = 0
+        try:
+            recall = TP / (TP + FN)
+        except:
+            recall = 0
+        try:
+            f1 = 2 * (precision * recall) / (precision + recall)
+        except:
+            f1 = 0
+        
         example['TP'] = TP
         example['FP'] = FP
         example['FN'] = FN
-
+        example['TN'] = TN
+        example['precision'] = precision
+        example['recall'] = recall
+        example['f1'] = f1
         return example
     
-    def extract_FP_FN_TP_token_by_token(self) -> (int, int, int):
+    def extract_FP_FN_TP_TN_token_by_token(self) -> (int, int, int):
         """
         Extract the number of False Positives, False Negatives and True Positives from the model output and the ground truth.
         Args:
@@ -47,40 +65,29 @@ class Evaluator():
         self.data = self.data.map(self._compare_prediction_label_one_example_token_by_token, batched=False)
 
 
-    def create_evaluation_table(self):
-        tmp_data = pd.DataFrame(self.data)
+    def create_evaluation_table(self, input_data:Dataset = None):
+        """
+        Create the evaluation table with the number of False Positives, False Negatives and True Positives.
+
+        Args:
+        input_data (Dataset): the dataset to evaluate. If None, the data used to initialize the class is used.
+
+        Returns:
+        dict: the evaluation table with the number of False Positives, False Negatives and True Positives.
+        """
+        if input_data is not None:
+            tmp_data = input_data
+        else:
+            tmp_data = pd.DataFrame(self.data)
         TP = tmp_data['TP'].sum()
         FP = tmp_data['FP'].sum()
         FN = tmp_data['FN'].sum()
+        TN = tmp_data['TN'].sum()
         precision = TP / (TP + FP)
         recall = TP / (TP + FN)
         f1 = 2 * (precision * recall) / (precision + recall)
-        self.evaluation_table = {'TP': TP, 'FP': FP, 'FN': FN,
+        self.evaluation_table = {'TP': TP, 'FP': FP, 'FN': FN, 'TN': TN,
                                   'precision':precision, 'recall':recall, 'f1':f1}
         
         return self.evaluation_table
     
-    def print_disallined_Is(self):
-        """
-        """
-        counter = 0
-        tot_tokens = 0
-        print('(token, prediction, ground_truth_label)')
-        self.disallined_df = pd.DataFrame(columns=['token', 'prediction', 'ground_truth_label'])
-        for example in self.data:
-            sentence_pred = example['predictions']
-            sentence = example['sentence']
-            previous = '' 
-            appened = False
-            for token in sentence_pred:
-                if token=='I' and previous=='O':
-                    appened = True
-                    counter+=1
-                    # print('token:', token, 'previous:', previous, 'position:', i, 'sentence:', sentence_pred)
-                previous = token
-                tot_tokens += 1
-            if appened:
-                tokenized_input = self.tokenizer(sentence, add_special_tokens=False)
-                tokens = self.tokenizer.convert_ids_to_tokens(tokenized_input["input_ids"])
-                self.disallined_df = pd.concat([self.disallined_df, pd.DataFrame([(t,p, ground_truth_label) for t, p, ground_truth_label in zip(tokens, sentence_pred, example['ground_truth_labels'])])])
-                # print([(t,p, ground_truth_label) for t, p, ground_truth_label in zip(tokens, sentence_pred, example['ground_truth_labels'])])
